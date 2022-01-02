5e0197cb9f0bf73f8140c14c2196fc00a494b289
Alex <zhialex.ru@gmail.com>
2022-01-02 20:26:36 +0300

    Fixed for the necessary selectors

--- src/test/java/hw2/tests/HasJunit5Code.java
+++ src/test/java/hw2/tests/HasJunit5Code.java
@@ -1,9 +1,9 @@
 package hw2.tests;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,7 +14,10 @@ public class HasJunit5Code {
     public void checkSoftAssertions() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $(By.linkText("Soft assertions")).shouldBe(visible).click();
-        $(byText("@ExtendWith")).parent().parent().shouldBe(visible);
+
+        $(by("data-filterable-for", "wiki-pages-filter"))
+                .lastChild().scrollTo().lastChild().click();
+        $(byText("SoftAssertions")).click();
+        $(byText("@ExtendWith")).scrollTo().shouldBe(visible);
     }
 }
